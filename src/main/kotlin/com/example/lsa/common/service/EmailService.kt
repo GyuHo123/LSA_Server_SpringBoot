bd1c014e2d6d4af8bf722065349bb1d0ce516a21
GyuHo123 <kyuhokim12@gmail.com>
2024-06-04 21:03:45 +0900

    EmailService의 이메일 주소를 공백이 아니라 환경변수로 설정하여 보호

--- src/main/kotlin/com/example/lsa/common/service/EmailService.kt
+++ src/main/kotlin/com/example/lsa/common/service/EmailService.kt
@@ -7,9 +7,10 @@ import org.springframework.stereotype.Service
 @Service
 class EmailService(private val mailSender: JavaMailSender) {
     fun sendEmail(to: String, subject: String, text: String) {
+        val fromAddress = System.getenv("EMAIL_FROM_ADDRESS")
         val message = mailSender.createMimeMessage()
         val helper = MimeMessageHelper(message, true)
-        helper.setFrom("")
+        helper.setFrom(fromAddress)
         helper.setTo(to)
         helper.setSubject(subject)
         helper.setText(text, true)

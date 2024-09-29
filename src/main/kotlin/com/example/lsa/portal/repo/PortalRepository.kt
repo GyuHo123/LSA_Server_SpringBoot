0a125f4168757c86dae0387083bbb115976ce39a
Kim Chaewon <101501925+Dim-chae@users.noreply.github.com>
2024-09-29 16:52:59 +0900

    Chore : 인터페이스 정의 완료
    PortalRepository JPA 레포지토리로 정의 완료

--- src/main/kotlin/com/example/lsa/portal/repo/PortalRepository.kt
+++ src/main/kotlin/com/example/lsa/portal/repo/PortalRepository.kt
@@ -1,8 +1,10 @@
 package com.example.lsa.portal.repo
 
+import com.example.lsa.portal.entity.SharedManual
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface PortalRepository {
+interface PortalRepository: JpaRepository<SharedManual, Long> {
 
 }
